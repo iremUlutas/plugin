9dd985c450935a8e542e2ad8da03720f792f6b11
norubai <orbay.altuntoprak@gmail.com>
2017-11-05 19:45:47 +0300

    bizim sey

---
+++ ihi.py
@@ -0,0 +1,15 @@
+import sublime
+import sublime_plugin
+
+
+class PepperCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if (self.view.is_read_only()):
+            self.view.set_read_only(False)
+            #self.view.set_status('toggle_readonly', '')
+            self.view.window().status_message('editleyiniz')
+            #self.window.set_menu_visible(False)
+        else:
+            self.view.set_read_only(True)
+            #self.view.set_status('toggle_readonly', 'Readonly')
+            self.view.window().status_message('riid onli')
